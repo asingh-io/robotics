612719c6fcd8e0e1374c5f4078b6cc30e3761b03
Faris Mudassir <farismudassir3@gmail.com>
2025-05-18 17:08:25 -0700

    starting with light show

---
+++ run_light_show.py
@@ -0,0 +1,30 @@
+from NexusDrive import *
+from NexusAttachement import *
+
+
+def run_mission(robo, arm):
+    # robo.straight_drive(100)
+    print("test")
+    #arm.move_left(300)
+    #arm.move_right(300)
+    # arm.move_right_time(300)
+
+    # arm.move_left_time(40)
+
+    # robo.pivot_turn(-76
+    robo.straight_drive(-147)
+    arm.move_left(307)
+    robo.pivot_turn(-29)
+    arm.move_left(-307)
+    arm.move_right(190)
+    arm.move_right(-190)
+    
+
+  
+    # robo.brake()
+
+
+if __name__ == "__main__":
+    robo = NexusDrive()
+    arm = NexusAttachement()
+    run_mission(robo, arm)
