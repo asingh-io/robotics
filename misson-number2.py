209a02e686a5527fb8fd30c5b5f170f7227490f2
Faris Mudassir <farismudassir3@gmail.com>
2025-06-15 15:26:32 -0700

    this week

--- misson-number2.py
+++ misson-number2.py
@@ -2,9 +2,25 @@ from NexusDrive import *
 from NexusAttachement import *
 
 
-def run_mission(robo, arm):
 
-   
+def run_mission(robo, arm):
+    robo.drive(-320)
+    arm.move_left(-250)
+
+    robo.drive(80)
+    robo.pivot_turn(-60)
+    robo.drive(-225)
+    robo.pivot_turn(45)
+    robo.drive(-390)
+    robo.pivot_turn(60)
+    robo.drive(15)
+    arm.move_left(-270)
+    robo.drive(15)
+    arm.move_left(250)
+    arm.move_left(-250)
+    
+    
+    
     robo.brake()
 
 
@@ -12,3 +28,5 @@ if __name__ == "__main__":
     robo = NexusDrive()
     arm = NexusAttachement()
     run_mission(robo, arm)
+
+
--- run_3dmovie.py
+++ run_3dmovie.py
@@ -3,23 +3,21 @@ from NexusAttachement import *
 
 
 def run_mission(robo, arm):
-    robo.straight_drive(-320)
+    robo.drive(-320)
     arm.move_left(-250)
 
-    robo.straight_drive(80)
+    robo.drive(80)
     robo.pivot_turn(-60)
-    robo.straight_drive(-225)
+    robo.drive(-225)
     robo.pivot_turn(45)
-    robo.straight_drive(-390)
+    robo.drive(-390)
     robo.pivot_turn(60)
-    robo.straight_drive(15)
+    robo.drive(15)
     arm.move_left(-270)
-    robo.straight_drive(15)
+    robo.drive(15)
     arm.move_left(250)
     arm.move_left(-250)
-    
-    
-    
+
     robo.brake()
 
 
